4aedfb75e642177fa952fd05cf68f27d6acb1875
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2024-05-21 18:38:01 -0300

    ejs

--- m_5_plataforma_pergunta/index.js
+++ m_5_plataforma_pergunta/index.js
@@ -1,16 +1,28 @@
 const express = require('express')
 const app = express()
 
-//Estou dizendo para o exoress usar o EJS como engine
+//Estou dizendo para o express usar o EJS como engine
 app.set('view engine', 'ejs')
 
 app.get('/:nome/:lang',(req, res) => {
     var nome = req.params.nome
     var lang = req.params.lang
+    //var exibirMsg = true
+
+    var produtos = [
+        {nome: "Doritos", preco: 3.14},
+        {nome: "coca-cola", preco: 5},
+        {nome: "leite", preco: 1.45},
+        {nome: "Carne", preco: 12.5},
+        {nome: "Suco", preco: 3.45},
+    ]
+
     res.render('index', {
         nome: nome,
         lang: lang,
-        empresa: 'Guia do programador'
+        empresa: 'Guia do programador',
+        //msg: exibirMsg,
+        produtos: produtos
     })
 })
 
