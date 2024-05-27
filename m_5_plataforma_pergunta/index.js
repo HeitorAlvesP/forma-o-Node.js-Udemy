cec3fc6c644bdffef6aa480912d50cd0df8eacca
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2024-05-27 15:21:05 -0300

    bootstrap

--- m_5_plataforma_pergunta/index.js
+++ m_5_plataforma_pergunta/index.js
@@ -3,27 +3,10 @@ const app = express()
 
 //Estou dizendo para o express usar o EJS como engine
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
-app.get('/:nome/:lang',(req, res) => {
-    var nome = req.params.nome
-    var lang = req.params.lang
-    //var exibirMsg = true
-
-    var produtos = [
-        {nome: "Doritos", preco: 3.14},
-        {nome: "coca-cola", preco: 5},
-        {nome: "leite", preco: 1.45},
-        {nome: "Carne", preco: 12.5},
-        {nome: "Suco", preco: 3.45},
-    ]
-
-    res.render('index', {
-        nome: nome,
-        lang: lang,
-        empresa: 'Guia do programador',
-        //msg: exibirMsg,
-        produtos: produtos
-    })
+app.get('/',(req, res) => {
+    res.render('index')
 })
 
 app.listen(8080,() => {console.log('Servidor On na PORT 8080')})
